9d57bb2461aca10a8a17712f066dc976bd6b7128
Sarah Weinstein <sbweins@gmail.com>
2025-04-26 10:52:21 -0400

    Week01

--- week_01_js_recap/problems.js
+++ week_01_js_recap/problems.js
@@ -1,16 +1,36 @@
-// Reverse a string
 function reverseString(str) {
-
+  let revStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    revStr += str[i];
+  }
+  return console.log(revStr);
 }
-  
-// Find max in array
+
+reverseString("Hello");
+
 function findMax(arr) {
-
+  let max;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < arr[i + 1]) {
+      max = arr[i] + 1;
+    }
+    max = arr[i];
+  }
+  return console.log(max);
 }
 
-// Check if palindrome
+findMax([1, 5, 3, 9]);
+
 function isPalindrome(word) {
-    
+  for (let i = 0; i < word.length / 2; i++) {
+    if (word[i] === word[word.length - 1 - i]) {
+      return console.log("true");
+    }
+    return console.log("false");
+  }
 }
+
+isPalindrome("racecar");
+isPalindrome("hello");
 
 module.exports = { reverseString, findMax, isPalindrome };
